535eed9001b37be6f00015561b92d8a3b165e81f
ssk <50195727+deviankim@users.noreply.github.com>
2025-01-24 21:41:53 +0900

    Update web_view_widget.dart

--- custom-actions-rest-api/web_view_widget.dart
+++ custom-actions-rest-api/web_view_widget.dart
@@ -1,3 +1,13 @@
+// Automatic FlutterFlow imports
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom widgets
+import '/custom_code/actions/index.dart'; // Imports custom actions
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class WebViewWidget extends StatefulWidget {
